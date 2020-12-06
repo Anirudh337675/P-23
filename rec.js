8644ff30c0e9502721c3104a17c9774138b36070
Anirudh337675 <73932473+Anirudh337675@users.noreply.github.com>
2020-12-06 11:48:36 +0530

    Add files via upload

---
+++ rec.js
@@ -0,0 +1,23 @@
+class Rec
+{
+    constructor(x,y,width,height)
+    {
+       var options={
+           isStatic:true,
+       }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+    display()
+    {
+        var roc=this.body.position;
+      
+        rectMode(CENTER);
+        fill("red");
+        rect(this.body.position.x,this.body.position.y, this.width,this.height);
+        
+    }
+
+}
